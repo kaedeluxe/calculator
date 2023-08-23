ad0712452137314e2ec029968ca95fdf0cf4ebbc
Kae <139962400+kaedeluxe@users.noreply.github.com>
2023-08-23 11:40:16 +0100

    basic functionality 90% complete
    +you can now add, subtract, divide and multiply correctly and clear the screen of values
    +rounded to 5 decimal places
    -if you keep mashing equals then something weird happens and everything breaks
    -backspace isn't implemented yet

--- script.js
+++ script.js
@@ -2,12 +2,33 @@ const query = (query) => document.querySelector(query),
   queryAll = (query) => document.querySelectorAll(query),
   createEl = (element) => document.createElement(element),
   screenText = query('.screenText'),
+  screenPrev = query('.screenPrev'),
   buttonsAll = queryAll('.buttonBackground'),
   inputDigits = queryAll('.digit'),
-  buttonClear = query('[data-input="C"]'),
-  buttonDecimal = query('.decimal');
+  buttonClearAll = queryAll('.clear');
+  buttonDecimal = query('.decimal'),
+  inputEquals = query('.equals'),
+  inputOperations = queryAll('.operation');
 
-let firstNumber, operation, secondNumber, screenValFirst, screenValCurrent;
+let firstNumber, operation, opCount = 0, secondNumber, screenValFirst, screenValCurrent = '', outputNumber;
+
+function clearScreen(clearType) {
+  switch (clearType) {
+    case "C":
+      screenText.textContent = '0';
+      screenValCurrent = '';
+      break;
+    case "CE":
+      screenText.textContent = '0';
+      screenPrev.textContent = '0';
+      screenValCurrent = '';
+      screenValFirst = '';
+      break;
+    case "Back":
+      screenText.textContent = 'idfk';
+      break;
+  };
+};
 
 inputDigits.forEach((button) => {
   button.addEventListener('click', () => {
@@ -16,18 +37,61 @@ inputDigits.forEach((button) => {
     } else {
       screenText.textContent += button.getAttribute('data-input');
     }
-    screenValCurrent = screenText.textContent;
+    screenValCurrent += button.getAttribute('data-input');
   });
 });
 
-buttonClear.addEventListener('click', () => {
-  screenText.textContent = '0';
+inputOperations.forEach((button) => {
+  button.addEventListener('click', () => {
+    if (opCount > 0) {
+      operate();
+    };
+    operation = button.getAttribute('data-input');
+    opCount++;
+    screenValFirst = screenValCurrent;
+    screenPrev.textContent = screenValFirst;
+    clearScreen('C');
+    screenValCurrent = '';
+    screenText.textContent = `${operation} `;
+  });
+});
+
+buttonClearAll.forEach((button) => {
+  button.addEventListener('click', () => {
+    clearScreen(button.getAttribute('data-input'));
+  });
 });
 
 buttonDecimal.addEventListener('click', () => {
-  if (screenText.textContent.includes('.')) {
+  if (screenValCurrent.includes('.')) {
     return;
   } else {
     screenText.textContent += '.';
-  }
+    screenValCurrent += '.';
+  };
 });
+
+function operate() {
+  firstNumber = Number(screenValFirst);
+  secondNumber = Number(screenValCurrent);
+  switch (operation) {
+    case "×":
+      outputNumber = firstNumber * secondNumber;
+      break;
+    case "÷":
+      outputNumber = firstNumber / secondNumber;
+      break;
+    case "+":
+      outputNumber = firstNumber + secondNumber;
+      break;
+    case "-":
+      outputNumber = firstNumber - secondNumber;
+      break;
+  };
+  outputNumber = Math.round(outputNumber * 100000) / 100000;
+  screenText.textContent = outputNumber;
+  screenValCurrent = outputNumber;
+  opCount = 0;
+};
+
+inputEquals.addEventListener('click', operate);
